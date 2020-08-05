bc74158bc801db6499253aaa89602e339aaadcb5
MateuszBorzyszkowski <boszmate@protonmail.com>
2020-08-05 09:40:25 +0200

    Add a calculator app with gui
    Project: calcguiexample
    
    Add a calculator app with gui

---
+++ calcguiexample/src/calc/Controller.java
@@ -0,0 +1,35 @@
+package calc;
+
+public class Controller {
+    public void clickButtonPlus() {}
+
+    public void clickButtonMinus() {}
+
+    public void clickButtonMultiply() {}
+
+    public void clickButtonDivision() {}
+
+    public void clickButtonOne() {}
+
+    public void clickButtonTwo() {}
+
+    public void clickButtonThree() {}
+
+    public void clickButtonFour() {}
+
+    public void clickButtonFive() {}
+
+    public void clickButtonSix() {}
+
+    public void clickButtonSeven() {}
+
+    public void clickButtonEight() {}
+
+    public void clickButtonNine() {}
+
+    public void clickButtonZero() {}
+
+    public void clickButtonDot() {}
+
+    public void clickButtonClear() {}
+}
